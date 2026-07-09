2f12ab4ceb732964cfcf67f56beecbc075fad35b
Ajitesh Manan Jha <147093114+ajiteshmananjha@users.noreply.github.com>
2026-07-09 15:09:05 +0530

    Time: 4 ms (23.25%), Space: 12.8 MB (39.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nxtgrt(nums2.size(),-1);
+
+        unordered_map<int, int> mp;
         vector<int> stk;
-        stk.push_back(0);
-        for(int i=1;i<nums2.size();i++)
+
+        for(int i = 0; i < nums2.size(); i++)
         {
-            if(nums2[i]>nums2[stk.back()])
+            while(!stk.empty() && nums2[i] > nums2[stk.back()])
             {
-                while(stk.size()!=0 && nums2[i]>nums2[stk.back()])
-                {
-                    nxtgrt[stk.back()]=nums2[i];
-                    stk.pop_back();
-                }
+                mp[nums2[stk.back()]] = nums2[i];
+                stk.pop_back();
             }
+
             stk.push_back(i);
         }
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            auto it = find(nums2.begin(),nums2.end(),nums1[i]);
-            int id=distance(nums2.begin(),it);
-            ans.push_back(nxtgrt[id]);
+        while(!stk.empty())
+        {
+            mp[nums2[stk.back()]] = -1;
+            stk.pop_back();
         }
+
+        vector<int> ans;
+
+        for(int x : nums1)
+        {
+            ans.push_back(mp[x]);
+        }
+
         return ans;
     }
 };
