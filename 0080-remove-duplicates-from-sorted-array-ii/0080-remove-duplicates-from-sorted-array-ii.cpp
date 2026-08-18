e4a181940236ed2201f3dc07002d9477c296ac30
Ajitesh Manan Jha <147093114+ajiteshmananjha@users.noreply.github.com>
2026-08-19 02:35:42 +0530

    Time: 9 ms (49.06%), Space: 19.5 MB (82.8%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0;
+        int r = 0;
+        unordered_map<int,int> freq;
+        // while(r < nums.size()){
+        //     if(nums[l] == nums[r] && count <= 2){
+        //         l++;
+        //         count++;
+        //     }
+        //     else if(nums[l] == nums[r] && count > 2)
+        //     {
+        //         count++;
+        //     }
+        //     else{
+        //         l++;
+        //         nums[l]=nums[r];
+        //     }
+        //     r++;
+        // }
+        int currentNum = nums[0];
+        int count = 0;
+        while(r < nums.size()){
+            if(nums[r] == currentNum) count++;
+            else{
+                currentNum = nums[r];
+                count = 1;
+            }
+            if(count <= 2){
+                nums[l] = nums[r];
+                l++;
+                r++;
+            }
+            else{
+                r++;
+            }
+        }
+        cout << l;
+        return l;
+    }
+};
